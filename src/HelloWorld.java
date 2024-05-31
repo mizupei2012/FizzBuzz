1bf0f42d700c4afa81cafcf96a09c8cb297d2eba
mizumura <166939231+mizupei2012@users.noreply.github.com>
2024-05-31 22:40:05 +0900

    '課題１完了'

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,8 +2,18 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-mizumura
+		for(int i=0;i<=100;i++){
+            if(i%5==0 && i%3==0){
+            	System.out.println("Fizzbuzz");
+            }else if(i%5==0){
+                System.out.println("buzz");
+            }else if(i%3==0){
+            	System.out.println("Fizz");
+                    }
+            else{
+                System.out.println(i);
+            }
+        };
 	}
 
 }
